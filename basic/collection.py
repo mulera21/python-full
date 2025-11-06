61fef9cf7ac0cd66c8bd4cfc2109bf729872e369
mulera <edmondmulera55@gmail.com>
2025-11-06 23:29:40 +0300

    list

---
+++ basic/collection.py
@@ -0,0 +1,13 @@
+# list
+empty_list = []  # or list()
+my_list = [1, 2, 3, 'four', 'five']
+
+print("List:", my_list)
+
+#get single item
+first_item = my_list[2]
+print("single item", first_item)
+# counting start from 0 in programming and -1 from the other end
+first_item = my_list[-1]
+#this will give last item which is five
+print("single item from last", first_item)
