5012bca0a3cdb99a4b9e04ba7e29cfd795295e8b
mulera <edmondmulera55@gmail.com>
2025-11-06 09:20:35 +0300

    string

---
+++ basic/01_basic_data_types.py
@@ -0,0 +1,40 @@
+#data types
+text = "text in python is called strings"  # string
+number = 42  # integer
+pi = 3.14  # float
+is_active = True  # boolean
+# printing data types
+
+print("string:", text)
+
+
+#Basic data types in Python
+string = 'text'
+num_int = 10
+num_float = 10.5
+boolean = False // True or False
+none_type = None  # represents absence of value
+
+print("String:", string)
+print("Integer:", num_int)
+print("Float:", num_float)
+print("Boolean:", boolean)
+print("NoneType:", none_type)
+print("Float:", pi)
+
+print(type(str))
+print(type(num_int))
+print(type(num_float))
+print(type(boolean))
+print(type(none_type))
+
+#👌👌👌👌simple excercise
+
+user = "Mike"
+app = "Revit"
+
+print("Once upon a time ,there was a " + app + " user named " + user + ".")
+print(user +" loved using " +app + " for his architectural designs.")
+print("Every day, " + user + " would explore new features in " + app + " to enhance his projects.")
+print("One day, "+ user + " discovered a powerful tool in " + app + " that transformed his workflow.")
+print("From that day on, " + user + " became a " + app + " expert, creating stunning designs with ease.")
