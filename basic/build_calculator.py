46da977e8118f928d7c08027489067e5715114c7
mulera <edmondmulera55@gmail.com>
2025-11-06 09:38:34 +0300

    building calculator

---
+++ basic/build_calculator.py
@@ -0,0 +1,12 @@
+# simple input
+
+num_a  = input("Enter first number: ")
+num_b  = input("Enter second number: ")
+
+# converting string input to float for calculation
+
+num_a = int(num_a)
+num_b = int(num_b)
+total = num_a + num_b
+
+print(f"The total is: {total}")
