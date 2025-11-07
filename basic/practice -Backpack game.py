f5c08a9d712416a8e9262235031c94c3375e705d
mulera <edmondmulera55@gmail.com>
2025-11-07 09:09:07 +0300

    backpack game

---
+++ basic/practice -Backpack game.py
@@ -0,0 +1,15 @@
+# Backpack Game
+# A simple text-based backpack game where the player can add, remove, and view items in their backpack.
+# starting with an empty backpack.
+backpack = []
+print("0. starting journey with an empty backpack.")
+
+
+print("🚨", backpack)
+print("--" * 50)
+print("1 . 😒picking up starterkit(Armor, shild, sword, potion).")
+backpack.append("Armor")
+backpack.append("Shield")
+backpack.append("Sword")
+backpack.append("Potion")
+print("🚨", backpack)
