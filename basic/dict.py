3c432651673c7aeb54a835ed8f1033cd7bbe515f
mulera <edmondmulera55@gmail.com>
2025-11-08 08:33:43 +0300

    dictionary

---
+++ basic/dict.py
@@ -0,0 +1,17 @@
+# Dictionary to store item quantities
+#😒😒
+print("--" * 50)
+
+phonebook = {
+    "Alice": "123-456-7890",
+    "Bob": "987-654-3210",
+    "Charlie": "555-555-5555"
+}
+
+#adding a new contact
+phonebook["David"] = "444-444-4444"
+phonebook["edward"] = "333-333-3333"
+print("Phonebook after adding contacts:", phonebook)
+
+number = phonebook["Alice"]
+print(f"📞📞calling Alice....({number})"
