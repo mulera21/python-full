8071d75e2e6886c5a557651ab737b6170bd88d12
mulera <edmondmulera55@gmail.com>
2025-11-08 09:10:11 +0300

    logic condition

---
+++ basic/logic.py
@@ -0,0 +1,18 @@
+# its time to add some logic
+
+#logic - syntax
+if 5 > 2:
+    print("5 is greater than 2")    
+
+#logic - syntax with if/elif/else
+
+temp = 30
+
+if temp >30:
+    print("It's a hot day")
+
+elif temp > 20:
+    print("It's a nice day")
+
+else:
+    print("It's a cold day")
