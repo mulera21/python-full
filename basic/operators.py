26e8fd71afd5d77271057bd4132b4a806c473bbb
mulera <edmondmulera55@gmail.com>
2025-11-08 08:51:33 +0300

    joing strings

--- basic/operators.py
+++ basic/operators.py
@@ -6,3 +6,18 @@ a = 10
 b = 5
 sum_result = a + b
 print("Addition:", sum_result)
+
+#joining strings
+greeting = "Hello, "
+name = "Alice"
+full_greeting = greeting + name
+print("String Concatenation:", full_greeting)
+
+name = 'Bob'
+print("New Name:", name)
+
+
+#multiply strings
+repeat_greeting = greeting * 3
+print("String Repetition:", repeat_greeting)
+
