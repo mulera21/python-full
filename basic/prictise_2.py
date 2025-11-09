b242a14106641a05c6270612e7a17c12bac263a6
mulera <edmondmulera55@gmail.com>
2025-11-09 08:23:09 +0300

    updated

--- basic/prictise_2.py
+++ basic/prictise_2.py
@@ -4,11 +4,12 @@ print("Welcome to the number guessing game")
 print("Guess a number between 0 and 20")
 print("------------------------------------------")
 
-user_1 = input("ENTER a number  ")
+secrete_number = 15
+user_1 = int(input("ENTER a number  "))
 #logic to check the number
-if int(user_1) >= 20:
-    print("too high repeat again😒")
-elif int(user_1) >= 5:
+if user_1 == secrete_number:
     print("correct guess✅")
+elif user_1 > 20:
+    print("too high repeat again✅")
 else:
     print("too low repeat again🚨")
