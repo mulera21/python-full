0b0bf29aaa6032fd8313307f9e36724a4fcf7265
mulera <edmondmulera55@gmail.com>
2025-11-09 08:55:31 +0300

    loop

--- basic/prictise_2.py
+++ basic/prictise_2.py
@@ -1,14 +1,15 @@
+import random
 #logic
 #number guessing game
 print("Welcome to the number guessing game")
 print("Guess a number between 0 and 20")
 print("------------------------------------------")
-
-secrete_number = 15
-
 #logic to check the number
 min = 1
 max = 20
+
+secrete_number = random.randint(min, max)
+
 for i in range(6):
     print(f"attempts left:, {i+1}/6")
 
