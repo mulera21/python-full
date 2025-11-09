ddd9846e0674583844dec5b21546fc354fa94d7d
mulera <edmondmulera55@gmail.com>
2025-11-09 08:12:53 +0300

    number game

--- basic/prictise_2.py
+++ basic/prictise_2.py
@@ -1,10 +1,14 @@
 #logic
+#number guessing game
+print("Welcome to the number guessing game")
+print("Guess a number between 0 and 20")
+print("------------------------------------------")
 
 user_1 = input("ENTER a number  ")
-
+#logic to check the number
 if int(user_1) >= 20:
-    print("too high repeat again✅")
+    print("too high repeat again😒")
 elif int(user_1) >= 5:
     print("correct guess✅")
 else:
-    print("too low repeat again✅")
+    print("too low repeat again🚨")
