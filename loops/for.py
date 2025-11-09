d9f6a3b5d0b43ea8c4fed6d6e745645f4018ade4
mulera <edmondmulera55@gmail.com>
2025-11-09 09:10:16 +0300

    iterating

--- loops/for.py
+++ loops/for.py
@@ -4,3 +4,17 @@ containers = ['apple', 'banana', 'cherry']
 
 for fruit in containers:
     print(fruit)
+
+#-----------------------------------------------------------
+materials = ['wood', 'metal', 'plastic']
+for material in materials:
+    print(material)
+
+#-----------------------------------------------------------
+#for loop in strings
+
+text = "Hello python"
+for char in text:
+    lower_char = char.lower()
+    upper_char = char.upper()
+    print(lower_char, upper_char)
---
+++ loops/loop_1.py
@@ -0,0 +1,8 @@
+#loop
+#---------------------------------------------
+#iterating using while loop
+
+big_number = 123456789
+
+for digit in str(big_number):
+    print(digit)
