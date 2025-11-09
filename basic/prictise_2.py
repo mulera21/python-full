db4ecdb8ef16f64255c12fe6ed6b822da3db2a35
mulera <edmondmulera55@gmail.com>
2025-11-09 08:08:39 +0300

    guess a number

---
+++ basic/prictise_2.py
@@ -0,0 +1,10 @@
+#logic
+
+user_1 = input("ENTER a number  ")
+
+if int(user_1) >= 20:
+    print("too high repeat again✅")
+elif int(user_1) >= 5:
+    print("correct guess✅")
+else:
+    print("too low repeat again✅")
