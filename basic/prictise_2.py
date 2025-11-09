112e832b4e862881b99b7f0420cfd5d06c61ee29
mulera <edmondmulera55@gmail.com>
2025-11-09 08:47:46 +0300

    for loop added on the game

--- basic/prictise_2.py
+++ basic/prictise_2.py
@@ -5,11 +5,21 @@ print("Guess a number between 0 and 20")
 print("------------------------------------------")
 
 secrete_number = 15
-user_1 = int(input("ENTER a number  "))
+
 #logic to check the number
-if user_1 == secrete_number:
-    print("correct guess✅")
-elif user_1 > 20:
-    print("too high repeat again✅")
-else:
-    print("too low repeat again🚨")
+min = 1
+max = 20
+for i in range(6):
+    print(f"attempts left:, {i+1}/6")
+
+    user_1 = int(input("ENTER a number  "))
+
+    if user_1 < min or user_1 > max:
+        print("out of range🚨incorrect number")
+    elif user_1 == secrete_number:
+        print("correct guess✅")
+        break
+    elif user_1 > secrete_number:
+        print("too high repeat again😒🚨")
+    else:
+        print("too low repeat again🚨")
