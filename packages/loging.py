54fdcaf4b200653fe1dddc02e292d6c13e2af428
mulera <edmondmulera55@gmail.com>
2025-11-10 15:49:35 +0300

    logging

--- packages/loging.py
+++ packages/loging.py
@@ -11,3 +11,14 @@ logging.warning("This is a warning message")
 logging.error("This is an error message")
 logging.critical("This is a critical message")
 logging.exception("This is an exception message")
+
+#simple example
+def divide(a, b):
+    try:
+        result = a / b
+        logging.info(f"Division successful: {a} / {b} = {result}")
+        return result
+    except ZeroDivisionError as e:
+        logging.error("Attempted to divide by zero.")
+        logging.exception("Exception occurred")
+        return None
