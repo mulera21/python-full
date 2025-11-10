19617fdd98e1c68ffbfec776a25c359a472c8072
mulera <edmondmulera55@gmail.com>
2025-11-10 08:46:43 +0300

    parameter

--- function/custom.py
+++ function/custom.py
@@ -3,3 +3,20 @@
 def say_hello():
     print("Hello,BMW !")
 say_hello()
+
+# 运行结果
+#function with arguments/parameters
+
+def greet_user(name = "Guest"):
+    print(f"Hello, {name}!")
+greet_user()
+
+
+# 运行结果
+# Hello, Guest!
+def greet_user(name):
+    print(f"Hello, {name}!")
+greet_user("Alice")
+
+
+
