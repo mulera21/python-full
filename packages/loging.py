3e05946a7138b8379c6f09d307c4720e149ac9c8
mulera <edmondmulera55@gmail.com>
2025-11-10 15:40:51 +0300

    logging

---
+++ packages/loging.py
@@ -0,0 +1,13 @@
+#loging
+import logging
+
+# Configure logging
+logging.basicConfig(level=logging.DEBUG)
+# loging messages
+
+logging.debug("This is a debug message")
+logging.info("This is an info message") 
+logging.warning("This is a warning message")
+logging.error("This is an error message")
+logging.critical("This is a critical message")
+logging.exception("This is an exception message")
---
+++ packages/sys.py
@@ -0,0 +1,9 @@
+#sys
+
+import sys
+
+def get_sys_version():
+    return sys.version
+
+def get_sys_platform():
+    return sys.platform
