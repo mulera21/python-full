2c07ea9bb8fb26bf9d5c1fdeb05e480d68cbacf4
mulera <edmondmulera55@gmail.com>
2025-11-10 08:09:15 +0300

    nested loop

--- loops/loop_1.py
+++ loops/loop_1.py
@@ -6,3 +6,20 @@ big_number = 123456789
 
 for digit in str(big_number):
     print(digit)
+
+#---------------------------------------------
+#nested for loop
+
+course = [['math', 'science', 'history'],
+          ['art', 'music', 'literature'],
+            ['physics', 'chemistry', 'biology']]
+
+for subject_list in course:
+    print(f"stating the module:{subject_list}")
+
+    for subject in subject_list:
+        print(f"completing subject: {subject}")
+
+        print("subject completed")
+    print("module completed")
+#---------------------------------------------
