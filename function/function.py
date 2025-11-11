348aef4e73a764d61067110dca36995880cc24f0
mulera <edmondmulera55@gmail.com>
2025-11-11 11:22:32 +0300

    function two parameter

--- function/function.py
+++ function/function.py
@@ -10,3 +10,12 @@ def show_message(msg):
     print("This is a sample function.", msg)
 
 show_message("Function executed successfully.")
+
+#--------------------------------function two parameter--------------------------------#
+
+def sum(num1, num2):
+    result = num1 + num2
+    print(f"The sum of {num1} + {num2} is: {result}")
+
+sum(10, 20)
+#--------------------------------function two parameter--------------------------------#
