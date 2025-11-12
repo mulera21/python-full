f12f2fa51ad26938a079d33a92eef342b6ea6e7e
mulera <edmondmulera55@gmail.com>
2025-11-12 08:53:02 +0300

    add functons on todo

--- function/todo.py
+++ function/todo.py
@@ -1,28 +1,37 @@
 #-----------to dolist-----------
 
-print("To Do List Application")
-print("---------To--Do--List----------")
-def show_task():
-        pass
+
+def show_task(task):
+    if task:
+        print("Your Tasks:")
+        for idx in task():
+            print(f"\t- {idx}")
+    else:
+        print("No tasks available.")    
 def add_task(task):
     pass
 def remove_task(task):
     pass
 task = []
-print("1. show Task")
-print("2. Add Task")
-print("3. Remove Task")
-print("4. Exit")
 
-option = int(input("Enter your choice (1-4): "))
 
-if option == 1:
-    show_task(task)
-elif option == 2:
-     add_task(task)
-elif option == 3:
-    remove_task(task)
-elif option == 4:
-     pass
-else:
-    print("Invalid option. Please choose a number between 1 and 4.")    
+while True:
+    print("To Do List Application")
+    print("---------To--Do--List----------")
+    print("1. show Task")
+    print("2. Add Task")
+    print("3. Remove Task")
+    print("4. Exit")
+
+    option = int(input("Enter your choice (1-4): "))
+
+    if option == 1:
+        show_task(task)
+    elif option == 2:
+         add_task(task)
+    elif option == 3:
+        remove_task(task)
+    elif option == 4:
+        break
+    else:
+        print("Invalid option. Please choose a number between 1 and 4.")    
