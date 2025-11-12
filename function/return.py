8a39d17dd0ecc538633e65540ff99f067e4d2c8c
mulera <edmondmulera55@gmail.com>
2025-11-12 09:30:05 +0300

    return type

--- function/return.py
+++ function/return.py
@@ -3,3 +3,9 @@ def add(a, b):
     return a + b
 result = add(3, 5)
 print(f"The sum is: {result}")
+
+#-------------return---type-----------
+def multiply(a:int, b:int) -> int:
+    return a * b
+result = multiply(4, 6)
+print(f"The product is: {result}")
