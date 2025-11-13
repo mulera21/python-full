c94798c484f07e04e484688a4825d60777066b18
mulera <edmondmulera55@gmail.com>
2025-11-13 09:20:00 +0300

    oop class constructor

--- oop/class.py
+++ oop/class.py
@@ -2,6 +2,8 @@
 #classes oop
 
 class Student:
+    def __init__(self, name):
+        self.name = name
     name = "John Doe" #class attribute
 
 student1 = Student() #create an object of the class
@@ -9,3 +11,7 @@ student2 = Student() #create another object of the class
 
 print(student1.name) #accessing class attribute
 print(student2.name) #accessing class attribute
+
+#--------------------------------method in class--------------------------------#
+class Person:
+    
