2610e031fb0f37b31fdcc573cce4a837cf8a4018
mulera <edmondmulera55@gmail.com>
2025-11-13 09:04:22 +0300

    oop class

--- oop/class.py
+++ oop/class.py
@@ -3,3 +3,9 @@
 
 class Student:
     name = "John Doe" #class attribute
+
+student1 = Student() #create an object of the class
+student2 = Student() #create another object of the class
+
+print(student1.name) #accessing class attribute
+print(student2.name) #accessing class attribute
