27872ec8d12fc99875eb66fcca84661d7ca0d568
mulera <edmondmulera55@gmail.com>
2025-11-14 08:27:49 +0300

    bank sytem

--- oop/bank.py
+++ oop/bank.py
@@ -1,12 +1,15 @@
 #--------------bank system-----------------
 
 class BankAccount:
+    def __init__(self, balance, account_holder):
+        self.balance = balance
+        self.account_holder = account_holder
 
     def deposit(self, amount):
         #addd money to bank account
         self.balance += amount
         print(f" &{amount} deposited. New balance: {self.balance}")
-        
+
 
     def withdraw(self):
         #remove money from bank account
