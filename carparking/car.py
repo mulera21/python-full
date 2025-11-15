956440a832286292a4ab164dd30816f948bf39a2
mulera <edmondmulera55@gmail.com>
2025-11-15 08:53:29 +0300

    car parking system

---
+++ carparking/car.py
@@ -0,0 +1,18 @@
+#-----------------car parking management system-----------------#
+
+class Car:
+    def __init__(self, license_plate, owner_name, parking_spot):
+        self.license_plate = license_plate
+        self.owner_name = owner_name
+        self.parking_spot = parking_spot
+
+    def get_details(self):
+        return {
+            "License Plate": self.license_plate,
+            "Owner Name": self.owner_name,
+            "Parking Spot": self.parking_spot
+        }
+
+car1 = Car("ABC123", "John Doe", "A1")
+car1_details = car1.get_details()
+print(car1_details)
