41962ac337436af9a995df17e632fedbe1b64a70
mulera <edmondmulera55@gmail.com>
2025-11-16 08:18:42 +0300

    add get methode

--- stuednt_system/student.py
+++ stuednt_system/student.py
@@ -1,3 +1,14 @@
 #----------------student management system-----------------#
 
 class Student:
+    def __init__(self, name , student_id, major):
+        self.name =name
+        self.student_id = student_id
+        self.major = major
+    
+    def get_details(self):
+        return {
+            "Name": self.name,
+            "Student ID": self.student_id,
+            "Major": self.major
+        }
