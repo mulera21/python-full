21dfefc1732ac259c17627960b46c407f1c67c5b
mulera <edmondmulera55@gmail.com>
2025-11-16 08:27:39 +0300

    print the enrollment method

--- stuednt_system/student.py
+++ stuednt_system/student.py
@@ -12,7 +12,14 @@ class Student:
             "Student ID": self.student_id,
             "Major": self.major
         }
-    
+    def enroll(self, course):
+        return f"Student {self.name} has enrolled in {course}."   
+
+print("------------Student Management System------------")
 student1 = Student("Alice Johnson", "S12345", "Computer Science")
 student1_details = student1.get_details()
+
+print("-----student1_details enrolled --------")
+enrollment_message = student1.enroll("Math 101")
 print(student1_details)
+print(enrollment_message)
