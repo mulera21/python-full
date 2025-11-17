1af03f74f9d82135e062f0e488caa5a1378dac71
mulera <edmondmulera55@gmail.com>
2025-11-17 16:21:18 +0300

    created a grade  method

--- oop/encapsulaton/student.py
+++ oop/encapsulaton/student.py
@@ -11,6 +11,17 @@ class Student:
         print(f"\tName: {self.name}")
         print(f"\tAge: {self.age}")
         print(f"\tGrade: {self.grade}")
+    def add_grade(self, grade):
+        if 0<grade<=100:
+            self.grade.append(grade)
+            print(f"Grade{grade} added for {self.grade}")
+        else:
+            print("invalid grade it shhould be 0 to 100")
+
+
+
+
 
 student1 = Student("Alice", 20,)
 student1.display_infor()
+student1.grade.append(100)
