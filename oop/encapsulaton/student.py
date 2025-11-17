e9fc45830fbe414c44396853cbf31ce7b7f4929c
mulera <edmondmulera55@gmail.com>
2025-11-17 11:08:06 +0300

    student management

--- oop/encapsulaton/student.py
+++ oop/encapsulaton/student.py
@@ -2,8 +2,12 @@
 
 class Student:
     def __init__(self, name, age, grade):
-        self.__name = name          # private attribute
-        self.__age = age            # private attribute
-        self.__grade = grade        # private attribute
+        self.name = name          # private attribute
+        self.ge = age            # private attribute
+        self.rade = []        # private attribute
 
-   
+    def display_infor(self):
+        print(f"Name: {self.name}, Age: {self.ge}, Grades: {self.rade}")
+
+student1 = Student("Alice", 20, "A")
+student1.display_infor()
