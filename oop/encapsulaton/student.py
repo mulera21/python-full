4b48f48ca78f1e4c277545507ae8a80688dc5f8a
mulera <edmondmulera55@gmail.com>
2025-11-17 16:13:02 +0300

    created a display method

--- oop/encapsulaton/student.py
+++ oop/encapsulaton/student.py
@@ -1,13 +1,16 @@
 #============encapsulation in Python============#
 
 class Student:
-    def __init__(self, name, age, grade):
+    def __init__(self, name, age):
         self.name = name          # private attribute
-        self.ge = age            # private attribute
-        self.rade = []        # private attribute
+        self.age = age            # private attribute
+        self.grade = []        # private attribute
 
     def display_infor(self):
-        print(f"Name: {self.name}, Age: {self.ge}, Grades: {self.rade}")
+        print("================student information===============")
+        print(f"\tName: {self.name}")
+        print(f"\tAge: {self.age}")
+        print(f"\tGrade: {self.grade}")
 
-student1 = Student("Alice", 20, "A")
+student1 = Student("Alice", 20,)
 student1.display_infor()
