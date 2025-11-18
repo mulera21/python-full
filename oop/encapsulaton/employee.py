22e689344e139eadc0212a1ced1b32269b1bebfb
mulera <edmondmulera55@gmail.com>
2025-11-18 08:25:52 +0300

    creating a method display

--- oop/encapsulaton/employee.py
+++ oop/encapsulaton/employee.py
@@ -4,3 +4,6 @@ class Employee:
     def __init__(self, name, salary):
         self.name = name
         self.salary = salary
+    
+    def display(self):
+        print(f"name is: {self.name} and the salary is: {self.salary}")
