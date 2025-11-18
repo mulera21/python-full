122a0230efed6a791f381b25e2344794c732f307
mulera <edmondmulera55@gmail.com>
2025-11-18 09:10:05 +0300

    accessing revenue

--- oop/encapsulaton/finance.py
+++ oop/encapsulaton/finance.py
@@ -2,14 +2,17 @@
 
 class Finance:
     def __init__(self):
-        self.revenue = 100000
+        self.__revenue = 100000 #private data
         self.number_of_sales = 114
+
+    def get_revenue(self):
+        return self.__revenue
 
 f1 = Finance()
 
 class HR:
     def __init__(self):
         self.number_of_emp= 32
-        print(f1.revenue)
+        print(f1.get_revenue())
 
 h1 =HR()
