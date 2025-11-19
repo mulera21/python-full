51d98f9cd60084f2c823f29c1611c74096949cbe
mulera <edmondmulera55@gmail.com>
2025-11-19 08:24:27 +0300

    display method

--- oop/encapsulaton/library.py
+++ oop/encapsulaton/library.py
@@ -1,4 +1,11 @@
 #------------------library manageent system----------------
 
 class Book:
+    def __init__(self, title, author, total_copies):
+        self.title = title
+        self.author = author
+        self.total_copies = total_copies
+        self.borrowed = 0
     
+    def display_info(self):
+        
