f6a2e7bc5e929cc91c6c10330d55bb9b54f3acbb
mulera <edmondmulera55@gmail.com>
2025-11-19 09:26:45 +0300

    creating encapsulation

--- oop/encapsulaton/library.py
+++ oop/encapsulaton/library.py
@@ -4,33 +4,35 @@ class Book:
     def __init__(self, title, author, total_copies):
         self.title = title
         self.author = author
-        self.total_copies = total_copies
-        self.borrowed = 0
+        self.__total_copies = total_copies
+        self.__borrowed = 0
     def borrow_book(self):
-        if self.borrowed<self.total_copies:
-            self.borrowed+=1
+        if self.__borrowed<self.__total_copies:
+            self.__borrowed+=1
             print(f"Borrowed 1 copy of {self.title}")
         else:
             print(f"Sorry all copies of {self.title} are currently borrowed")
 
     def return_book(self):
-        if self.borrowed>0:
-            self.borrowed-=1
+        if self.__borrowed>0:
+            self.__borrowed-=1
             print(f"Return 1 copy of {self.title}")
         else:
-            print(f"No copies of {self.borrowed}")
+            print(f"No copies of {self.__borrowed}")
     
     def display_info(self):
         print(f"----------Book info----------")
         print(f"\tTitle: {self.title}")
         print(f"\tAuthor: {self.author}")
-        print(f"\tTotal copies: {self.total_copies}")
-        print(f"Borrowed Copies: {self.borrowed}")
+        print(f"\tTotal copies: {self.__total_copies}")
+        print(f"Borrowed Copies: {self.__borrowed}")
 
 book = Book("Alissa", "c.j more", 7 )
 
 book.display_info()
 book.borrow_book()
+book.__total_copies = 10
+book.__borrowed = 15
 book.borrow_book()
 book.borrow_book()
 book.borrow_book()
