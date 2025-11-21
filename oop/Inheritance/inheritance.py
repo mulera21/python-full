492161316b5e627a20cd81e566fdc028ba862a66
mulera <edmondmulera55@gmail.com>
2025-11-21 08:38:14 +0300

    display method

--- oop/Inheritance/inheritance.py
+++ oop/Inheritance/inheritance.py
@@ -4,3 +4,6 @@ class Person:
     def __init__(self, name , age):
         self.name = name
         self.age = age
+
+    def display_info(self):
+        print(f"{self.name} is {self.age} years old")
