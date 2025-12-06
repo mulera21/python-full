ddd31ead92ed2d411eb1ba363ea08dec2aba72fe
mulera <edmondmulera55@gmail.com>
2025-12-06 23:13:44 +0300

    metod overloading

--- polymorphism/overloading.py
+++ polymorphism/overloading.py
@@ -3,3 +3,7 @@
 class MathOperations:
     def add(self, a, b, c=0):
         return a + b + c
+# Example usage
+math_ops = MathOperations()
+print(math_ops.add(2, 3))        # Output: 5
+print(math_ops.add(2, 3, 4))     # Output: 9
